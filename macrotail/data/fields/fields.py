e263d6a360fb6900b09fd91ea1b88416732da894
hwzeng <zhw1025@gmail.com>
2018-10-24 09:47:31 +0800

    intial comments

---
+++ macrotail/data/fields/fields.py
@@ -0,0 +1,11 @@
+class Fields():
+    def __init__(self):
+        pass
+
+    def preprocess(self):
+        return NotImplementedError
+
+    def process(self):
+        return NotImplementedError
+
+    def numerial(self):
---
+++ macrotail/data/fields/visual_dialog_fields.py
@@ -0,0 +1,9 @@
+from fields import Fields
+
+
+class VisualDialogFields(Fields):
+    def preprocess(self):
+        pass
+
+    def process(self):
+        pass
